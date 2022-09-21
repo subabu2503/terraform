1787ce98705cfc19d5c10c9a89cf27203ee26f53
Suresh Warrier <48576944+subabu2503@users.noreply.github.com>
2022-09-21 06:16:41 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstates"
-    storage_account_name = "kstfstateaccount"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
   }
 }
