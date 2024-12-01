b93edbb94394adce9b1b5004466f52086039bcb9
Suresh Warrier <48576944+subabu2503@users.noreply.github.com>
2024-12-01 13:11:48 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,26 +10,4 @@ resource "azurerm_resource_group" "default" {
     environment = "Production"
   }
 }
-/*
-resource "azurerm_container_registry" "acr" {
-  name                = "kspcontainerregistry"
-  resource_group_name = azurerm_resource_group.default.name
-  location            = "East US 2"
-  sku                 = "Standard"
-  admin_enabled       = true
-}
 
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "acr_admin_username" {
-  value     = azurerm_container_registry.acr.admin_username
-  sensitive = true
-}
-
-output "acr_admin_password" {
-  value     = azurerm_container_registry.acr.admin_password
-  sensitive = true
-}
-#/
