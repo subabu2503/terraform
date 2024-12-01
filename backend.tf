ecf6a8009ea283c254017c6f3e267a02592c8aa8
Suresh Warrier <48576944+subabu2503@users.noreply.github.com>
2024-12-01 13:09:46 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = ""
+    resource_group_name  = "rg-tf-backend"
+    storage_account_name = "sbwstaccounttfbackend"
+    container_name       = "terraform-state-github"
     key                  = ""
   }
 }
